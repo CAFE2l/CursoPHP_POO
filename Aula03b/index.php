93fcc0e409ce4e43c596c6f78670346fb516e2b8
CAFÉ <pciskolargx@gmail.com>
2025-10-29 14:00:27 -0300

    Aula03b finalizada

--- Aula03b/index.php
+++ Aula03b/index.php
@@ -8,7 +8,19 @@
 </head>
 <body>
 <div>
-    
+    <pre>
+        <?php
+            require_once 'Caneta.php';
+            $c1 = new Caneta;
+            $c1->modelo = "BIC cristal";
+            // $c1->cor = "Azul";
+            // $c1->ponta = 0.5; //atributo privado
+            // $c1->tampada; //atributo protegido
+            print_r($c1);
+            $c1->rabiscar();
+            $c1->tampar();
+        ?>
+    </pre>
 </div>
 </body>
 </html>
