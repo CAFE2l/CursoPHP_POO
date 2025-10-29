420f588e28d88a368d3b6a1390645239ec206d62
CAFÉ <pciskolargx@gmail.com>
2025-10-29 14:53:25 -0300

    parei no minuto 9:00

--- Aula04b/index.php
+++ Aula04b/index.php
@@ -13,6 +13,10 @@
            require_once 'caneta.php';
 
            $c1 = new Caneta();
+        //    $c1->setModelo("BIC");
+           $c1->modelo = "BIC";
+        //    $c1->setPonta(0.5);
+             $c1->ponta = 1.0;
            print_r($c1);
         ?>
     </pre>
