f40770d1b595dc4ca33319c9c5d715fe17f33413
CAFÉ <pciskolargx@gmail.com>
2025-10-29 14:49:14 -0300

    salvando o começo da aula 4

---
+++ Aula04b/caneta.php
@@ -0,0 +1,23 @@
+<?php 
+
+    class Caneta{
+        public $modelo;
+        private $ponta;
+        public function getModelo(){
+             return $this->modelo;
+        }
+
+        public function setModelo($modelo){
+            $this->modelo = $modelo;
+        }
+
+        public function getPonta(){
+            return $this->ponta;
+        }
+        public function setPonta($ponta){
+            return $this->ponta = $ponta;
+        }
+    }   
+
+
+?>
