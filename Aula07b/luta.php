6233d7e91529dceba5ef332fe16ccd5a5e00e10c
CAFÉ <pciskolargx@gmail.com>
2025-11-04 18:43:38 -0300

    encerramos a luta

--- Aula07b/luta.php
+++ Aula07b/luta.php
@@ -8,12 +8,47 @@ require_once "lutador.php";
         private $aprovada;
 
         //Métodos públicos
-        public function marcarLuta(){
+        public function marcarLuta($l1, $l2){
+            if ($l1.getCategoria() == $l2.getCategoria() && ($l1 != $l2)){
+                $this->aprovada = true;
+                $this->desafiado = $l1;
+                $this->desafiante = $l2;
 
+            } else{
+                $this->aprovada = false;
+                $this->desafiado = null;
+                $this->desafiante = null;
+            }
         }
 
         public function lutar(){
+            if($this->aprovada){
+                $this->desafiado.apresentar();
+                $this->desafiante.apresentar();
 
+                $vencedor = rand(0,2);
+
+                switch($vencedor){
+                    case 0:
+                        break;
+                        echo "EMPATE";
+                        $this->desafiado.empatarLuta();
+                        $this->desafiante.empatarLuta();
+
+                    case 1:
+                        echo "<p>".$this->desafiado->getNome()." venceu</p>";
+                        $this->desafiado.perderLuta();
+                        $this->desafiante.ganharLuta();
+
+                    case 2:
+                        echo "<p>".$this->desafiante->getNome()." venceu</p>";
+                        $this->desafiado.ganharLuta();
+                        $this->desafiante.perderLuta();
+                        break;
+                    }
+            } else{
+                echo"<p>Luta não pode acontecer</p>";
+            }
         }
 
         //Métodos especiais
