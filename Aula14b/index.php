cf555eb86cad5fc3f3e708823aff8dd76336e3ec
CAFÉ <pciskolargx@gmail.com>
2025-11-17 15:03:57 -0300

    nao sei o que deu de errado :/

--- Aula14b/index.php
+++ Aula14b/index.php
@@ -15,11 +15,8 @@
        require_once 'AcoesVideo.php';
        require_once 'Pessoa.php';
        
-        $v[0] = new Video("Aula 1 de Poo");
-        $v[1] = new Video("Aula 2 de PHP");
-        $v[2] = new Video("Aula 3 de HTML & CSS");
-       
-        print_r($v);
+        $g[0] = new Gafanhoto("Jubileu", 22, "M", "Juba");
+        print_r($g);
        ?>
     </pre>
 </div>
